4fbe4f798ebc9567cf7a741eda82bc40b10cf249
mayukh45 <mayukh1110@gmail.com>
2019-04-12 16:53:19 +0530

    Added BusParser

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("\n".join(["lol"+str(i) for i in range(5)]))
+import collections
+d = {'dir' : 1 , "lol" :{"dir" : 1 , "wtf":{"negro" : 2 , "dir" : 3}}}
+
+
+def update(u):
+    for k, v in u.items():
+        if isinstance(v, collections.Mapping):
+            u[k] = update(u.get(k))
+            print(k)
+        else:
+            u['dir'] = 0
+    return u
+print(update(d))
