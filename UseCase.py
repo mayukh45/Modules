066eabf3c88e5865025c14c097e6f7ae078ff856
mayukh45 <mayukh1110@gmail.com>
2019-04-20 00:47:58 +0530

    Sync

--- UseCase.py
+++ UseCase.py
@@ -1,8 +1,8 @@
 from BusParser import BusParser
 trial = BusParser("astob.yaml" ,"astob")
 trial.remove_sub_dict("astob.snoop")
-trial.add_super_node("astob.wr.ingress")
-trial.add_super_node("astob.rd.ingress")
+trial.add_super_node("astob.wr","ingress")
+trial.add_super_node("astob.rd","ingress")
 trial.flip_op("astob.ingress")
 trial.copy("astob.ingress" , "egress0")
 trial.copy("astob.egress0" , "egress1")
