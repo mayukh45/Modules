db893e878a0ebdbd6242123cf3e280270bc23a07
mayukh45 <mayukh1110@gmail.com>
2019-04-24 00:07:17 +0530

    Adding tests

--- BusParserUnitTest.py
+++ BusParserUnitTest.py
@@ -1,32 +1,19 @@
 from BusParser import BusParser
 import copy
-trial = BusParser("astob.yaml" ,"astob")
+trial = BusParser("/home/mayukhs/Dpcuments/refbuses/astob_heavy.yaml","astob")
 temp = copy.deepcopy(trial.dict["astob"]["snoop"])
 
 trial.dyaml("loaded.yaml")
 
-
-trial.remove_sub_dict("astob.snoop")
-trial.add_super_node("astob.wr","ingress")
-trial.add_super_node("astob.rd","ingress")
-trial.flip_op("astob.ingress")
-
-trial.copy("astob.ingress" , "egress0")
-trial.copy("astob.egress0" , "egress1")
-trial.copy("astob.egress0" , "egress2")
-trial.copy("astob.egress0" , "egress3")
+trial.wid_op_flat("wdata", 10)
+trial.wid_op_flat("rdata" ,10)
+trial.remove_node_flat("heir1")
+trial.add_sub_dict_flat("snoop" , {"hello" : "world"})
+trial.prefixop_flat("sdata","usb2usb")
+trial.prefixop_flat("sdata", "usb2cpu")
+trial.flip_op_flat("wvalid")
 trial.dyaml("trial.yaml")
-trial.remove_sub_dict("astob.egress0")
-trial.remove_sub_dict("astob.egress1")
-trial.remove_sub_dict("astob.egress2")
-trial.remove_sub_dict("astob.egress3")
-#trial.dyaml("trial1.yaml")
-trial.remove_node("astob.ingress")
-trial.dyaml("trial1.yaml")
 
 
-trial.add_sub_dict("astob.snoop",temp)
-trial.flip_op("astob.snoop")
-trial.flip_op("astob")
 
-trial.dyaml("**astob.yaml")
+#trial.dyaml("**astob.yaml")
