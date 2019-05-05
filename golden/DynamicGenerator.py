2d2eaf0293c0a60a37db258ac273db46c82b0187
mayukh45 <mayukh1110@gmail.com>
2019-05-05 21:17:32 +0530

    Sync

--- golden/DynamicGenerator.py
+++ golden/DynamicGenerator.py
@@ -2,19 +2,19 @@
 # Class Dynamic Generator - The chief functionality of this class to be able to take a piece
 # of code as string and able to run it as any other python code and return the output string
 # We achieve this today by writing into py file and getting another output file and write
-# back to main file. 
+# back to main file.
 # TODO: In future, find out a better way to achieve this.
 #=============================================================================================
 
 import os
-from datetime import datetime   #### import datatime 
+from datetime import datetime   #### import datatime
 class DynamicGenerator:
     """
     body_data -> actual template means if we go for LruArbiter module, we are passing body template of LruArbiter module.
     dic -> dictonary of variable we are using in Module, it is used when we are creating temporary python file.
     """
     def __init__(self,dic,body_data):
-        self.body_data=body_data            
+        self.body_data=body_data
         self.dic=dic
         self.temp_py_file=""
         self.temp_txt_file=""
@@ -22,7 +22,7 @@ class DynamicGenerator:
     def parse_body(self):
         main_body=""
         split_body=self.body_data.split("/f_f/")    # /f_f/ is an expression, based on this we are splitinng the body into a list.
-        
+
         for b in range(len(split_body)):
             """
             if the index is odd then we process odd position code, dumped into pyhton code then process into text file.
@@ -30,7 +30,7 @@ class DynamicGenerator:
 
             """
             if b%2==0:
-                main_body+=split_body[b] 
+                main_body+=split_body[b]
             else:
                 main_body+=self.temporary_pyfile(self.dic,split_body[b])
         return main_body
@@ -62,13 +62,13 @@ class DynamicGenerator:
         return lines
 
     """
-    after processing all operation removing all temporary file  
+    after processing all operation removing all temporary file
     """
     def silentremove(self):
         try:
             # os.remove(self.temp_py_file)
             # os.remove(self.temp_txt_file)
-            os.system("rm temp_*")
+            #os.system("rm temp_*")
             os.system("rm output_*")
         except:
             raise Exception("file not found")
